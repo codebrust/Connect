51b88d139716d9877121a1701b5c7607d6c5fefc
Kisan Thapa <kisanthapa33@gmail.com>
2018-01-19 11:13:00 +0545

    Initial commit. This is the first commit of this project.

---
+++ app/src/main/java/com/example/kisanthapa/connect/ChatList.java
@@ -0,0 +1,25 @@
+package com.example.kisanthapa.connect;
+
+/**
+ * Created by Kisan Thapa on 10/17/2017.
+ */
+
+public class ChatList {
+
+    private String date;
+
+    public ChatList() {
+
+    }
+    public ChatList(String date) {
+        this.date = date;
+    }
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+}
